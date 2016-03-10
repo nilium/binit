4ce41f88cbcf5bf132e96301dbfb9a3adacc9019
Noel Cower <ncower@gmail.com>
2016-03-10 13:20:55 -0800

    Initial commit
    Change-Id: I518fdffeeeb9774a1e253584a925d827a00c7b6c

---
+++ main.go
@@ -0,0 +1,110 @@
+// Command binit is an env-like tool to exec programs. In addition to being able to use or discard the current
+// environment and pass environment variables on the command line, they may also be loaded from .ini files (as passed in
+// via the -f option).
+//
+// For example:
+//
+//   $ binit -e thing.var=value -f config.ini -i env
+//   export section.key="value"
+//   export section.with-newlines="value
+//   with
+//   newlines"
+//   export thing.var="value"
+//
+package main
+
+import (
+	"flag"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"syscall"
+
+	stdlog "log"
+
+	"go.spiff.io/go-ini"
+)
+
+type Strings []string
+
+func (s *Strings) String() string {
+	return "[]"
+}
+
+func (s *Strings) Set(str string) error {
+	*s = append(*s, str)
+	return nil
+}
+
+func log(args ...interface{}) { stdlog.Print(args...) }
+
+func main() {
+	stdlog.SetPrefix("binit: ")
+	stdlog.SetFlags(0)
+
+	var env []string
+
+	clean := flag.Bool("i", false, "Whether to omit current environment variables from the exec.")
+	var inputs = new(Strings)
+
+	flag.Var((*Strings)(&env), "e", "Set an environment variable (`K=V`).")
+	flag.Var(inputs, "f", "INI `file`s to load into the environment. (Pass - to read from standard input.)")
+
+	flag.Parse()
+
+	var values map[string]string
+	var err error
+	for _, fp := range *inputs {
+		var b []byte
+
+		if fp == "-" {
+			b, err = ioutil.ReadAll(os.Stdin)
+		} else {
+			b, err = ioutil.ReadFile(fp)
+		}
+
+		if err != nil {
+			log("error reading <", fp, ">:", err)
+			continue
+		}
+
+		values, err = ini.ReadINI(b, values)
+		if err != nil {
+			log("error parsing INI ", fp, ": ", err)
+		}
+	}
+
+	for k, v := range values {
+		pair := k + "=" + v
+		env = append(env, pair)
+	}
+
+	if !*clean {
+		env = append(os.Environ(), env...)
+	}
+
+	argv := flag.Args()
+	if len(argv) == 0 {
+		for _, pair := range env {
+			io.WriteString(os.Stdout, pair+"\n")
+		}
+		return
+	}
+
+	cmd, err := exec.LookPath(argv[0])
+	if err != nil {
+		log(err)
+		os.Exit(127)
+	}
+
+	argv[0] = cmd
+
+	if err := syscall.Exec(cmd, argv, env); err != nil {
+		log("error exec-ing to <", cmd, ">: ", err)
+		os.Exit(126)
+	}
+
+	log("exec failed, process still running")
+	os.Exit(1)
+}
